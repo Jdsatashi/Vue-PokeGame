5adc253fcc7ffbaa58b9be400181e5a12e5b6fdb
jdsatashi <vdt073@gmail.com>
2023-08-18 11:53:32 +0700

    Upload sometimes

--- src/components/CardGame.vue
+++ src/components/CardGame.vue
@@ -1,28 +1,67 @@
-<script setup>
-import { ref } from 'vue'
-    const isFlipped = ref(false)
-    const onToggleFlipCard = () =>{
-        isFlipped.value = !isFlipped.value
-    }
-// eslint-disable-next-line no-unused-vars
-    const props = defineProps({
-        imgBackFaceUrl: {
-            type: String,
-            required: true,
+/*
+import { ref } from "vue";
+
+const emits = defineEmits(["flip"]);
+
+const isFlipped = ref(false);
+
+// eslint-disable-next-line vue/valid-define-props
+const { card, imgBackFaceUrl } = defineProps({
+card: [String, Number, Array, Object],
+imgBackFaceUrl: String
+});
+
+const onToggleFlipCard = () => {
+isFlipped.value = !isFlipped.value;
+if (isFlipped.value) {
+emits("flip", card);
+}
+};
+const onFlipBackCard = () => {
+isFlipped.value = false;
+}
+*/
+<script>
+    export default {
+        props: {
+            card: {
+                type: [String, Number, Array, Object]
+            },
+            imgBackFaceUrl: {
+                type: String,
+                required: true
+            }
+        },
+        data() {
+            return {
+                isFlipped: false
+            };
+        },
+
+        methods: {
+            onToggleFlipCard() {
+                this.isFlipped = !this.isFlipped;
+                if(this.isFlipped) this.$emit("flip", this.card)
+            },
+            onFlipBackCard(){
+                this.isFlipped = false
+            }
         }
-    })
-const imgUrl = props.imgBackFaceUrl
+    }
 </script>
 
 <template>
     <div class="card">
-        <div class="card-inner" :class="{'is-flipped': isFlipped}" v-on:click="onToggleFlipCard">
+        <div class="card-inner"
+             :class="{'is-flipped': isFlipped}"
+             v-on:click="onToggleFlipCard"
+        >
             <div class="card-face card_face-font">
                 <div class="card-content"></div>
             </div>
             <div class="card-face card_face-back">
                 <div class="card-content"
-                     :style="{ backgroundImage: `url('/src/assets/${imgUrl}')`}">
+                     :style="{ backgroundImage: `url('/src/assets/${imgBackFaceUrl}')`}">
                 </div>
             </div>
         </div>
@@ -30,49 +69,55 @@ const imgUrl = props.imgBackFaceUrl
 </template>
 
 <style lang="css" scoped>
-    .card {
-        display: inline-block;
-        margin-right: 1rem;
-        margin-bottom: 1rem;
-        width: 90px;
-        height: 120px;
-    }
-    .card-inner{
-        width: 100%;
-        height: 100%;
-        transition: transform 1s;
-        transform-style: preserve-3d;
-        cursor: pointer;
-        position: relative;
-    }
-    .card-inner.is-flipped {
-        transform: rotateY(-180deg);
-    }
-    .card-face {
-        position: absolute;
-        width: 100%;
-        height: 100%;
-        backface-visibility: hidden;
-        overflow: hidden;
-        border-radius: 1rem;
-        padding: 1rem;
-        box-shadow: 0 3px 10px 3px rgba(0,0,0,0.3);
-    }
-    .card_face-font .card-content {
-        background: url("../assets/images/icon_back.png") no-repeat center center;
-        background-size: 40px 40px;
-        width: 100%;
-        height: 100%;
-    }
-    .card_face-back {
-        background-color: var(--light);
-        transform: rotateY(-180deg);
-    }
-    .card_face-back .card-content {
-        background-size: contain;
-        background-position: center center;
-        background-repeat: no-repeat;
-        height: 100%;
-        width: 100%;
-    }
+.card {
+    display: inline-block;
+    margin-right: 1rem;
+    margin-bottom: 1rem;
+    width: 90px;
+    height: 120px;
+}
+
+.card-inner {
+    width: 100%;
+    height: 100%;
+    transition: transform 1s;
+    transform-style: preserve-3d;
+    cursor: pointer;
+    position: relative;
+}
+
+.card-inner.is-flipped {
+    transform: rotateY(-180deg);
+}
+
+.card-face {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    backface-visibility: hidden;
+    overflow: hidden;
+    border-radius: 1rem;
+    padding: 1rem;
+    box-shadow: 0 3px 10px 3px rgba(0, 0, 0, 0.3);
+}
+
+.card_face-font .card-content {
+    background: url("../assets/images/icon_back.png") no-repeat center center;
+    background-size: 40px 40px;
+    width: 100%;
+    height: 100%;
+}
+
+.card_face-back {
+    background-color: var(--light);
+    transform: rotateY(-180deg);
+}
+
+.card_face-back .card-content {
+    background-size: contain;
+    background-position: center center;
+    background-repeat: no-repeat;
+    height: 100%;
+    width: 100%;
+}
 </style>
--- src/components/InteractScene.vue
+++ src/components/InteractScene.vue
@@ -1,18 +1,79 @@
-<script setup>
-    import CardGame from "@/components/CardGame.vue";
+/*
+import CardGame from "@/components/CardGame.vue";
+import { ref } from "vue";
 
-    defineProps({
+const rules = ref([]);
+
+defineProps({
+cardContext: {
+type: Array,
+default: () => []
+}
+});
+
+const checkRule = (card) => {
+let rule = rules.value;
+if (rule.length === 2) return false;
+rule.push(card);
+
+if (rule.length === 2 && rule[0].value === rule[1].value) {
+console.log("true...");
+} else if (rule.length === 2 && rule[0].value !== rule[1].value) {
+console.log("Wrong...");
+ref.value[`card-${rule[0].index}`].onFlipBackCard();
+ref.value[`card-${rule[1].index}`].onFlipBackCard();
+} else return false;
+};
+*/
+<script>
+import CardGame from "@/components/CardGame.vue";
+
+export default {
+    props: {
         cardContext: {
             type: Array,
-            default: () => []
+            default: function () { return [] }
         }
-    })
+    },
+    components: {
+        CardGame
+    },
+    data() {
+        return {
+            rules: []
+        };
+    },
+    methods: {
+        checkRule(card){
+            console.log(card)
+            if(this.rules.length === 2) return false
+            this.rules.push(card)
+
+            if(this.rules.length === 2 && this.rules[0].value === this.rules[1].value){
+                console.log("true...")
+            } else if(this.rules.length === 2 && this.rules[0].value !== this.rules[1].value){
+                console.log("Wrong...")
+                this.$refs[`card-${this.rules[0].index}`][0].onFlipBackCard();
+                this.$refs[`card-${this.rules[1].index}`][0].onFlipBackCard();
+
+                this.rules = []
+            } else return false
+        }
+    }
+};
 </script>
 
 <template>
     <div class="scene">
         <h1>Interact Component here</h1>
-        <CardGame v-for="(card, index) in cardContext" :key="index"  :img-back-face-url="`images/${card}.png`"/>
+        <CardGame v-for="(card, index) in cardContext"
+                  :key="index"
+                  :ref="`card-${index}`"
+                  :img-back-face-url="`images/${card}.png`"
+                  v-bind:card="{ index, value: card }"
+                  :rules="rules"
+                  @flip="checkRule($event)"
+        />
     </div>
 </template>
 
